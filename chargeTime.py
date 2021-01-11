13813be7ba03d2d52833e1b1ff6894871dd9e955
dglass710 <66642674+dglass710@users.noreply.github.com>
2021-01-11 10:11:42 -0600

    Add files via upload

---
+++ chargeTime.py
@@ -0,0 +1,70 @@
+import time, HumanTime
+
+def mainH(initialPercentage, capacity):
+    initialTime = time.time()
+    UI = True
+    while UI:
+        tmp = input('Enter done when finished:\t')
+        if tmp.lower() == 'done':
+           UI = False
+    finalTime = time.time()
+    tmp = input('What is the new percentage?\t')
+    UI = True
+    lastWasNumeric = False
+    while UI:
+        try:
+            finalPercentage = eval(tmp)
+            lastWasNumeric = True
+            while finalPercentage < initialPercentage:
+                if lastWasNumeric:
+                    tmp = input("Your final percentage was lower than your initial percentage.\nWhat is the new percentage?\t")
+                else:
+                    tmp = input("Your input cannot be understood.\nWhat is the new percentage?\t")
+                try:
+                    finalPercentage = eval(tmp)
+                    lastWasNumeric = True
+                except:
+                    lastWasNumeric = False
+            UI = False
+        except:
+            tmp = input('Your value was non-numerical.\nWhat is the new percentage?\t')
+    totalTimeElapsed = finalTime - initialTime
+    deltaPerc = finalPercentage - initialPercentage
+    totalTime = (totalTimeElapsed)/((deltaPerc)/100)
+    totalTimeElapsedStr = HumanTime.TimeAutoShort(finalTime - initialTime, 2)
+    majorityTotalTime = .7*totalTime
+    error = totalTime/(finalPercentage - initialPercentage)
+    majorityError = totalTime/((finalPercentage - initialPercentage)*10/7)
+    totalTimeStr = HumanTime.TimeAutoShort(totalTime, 2)
+    majorityTimeStr = HumanTime.TimeAutoShort(majorityTotalTime, 2)
+    errorStr = HumanTime.TimeAutoShort(error, 2)
+    majorityErrorStr = HumanTime.TimeAutoShort(majorityError, 2)
+    bestCase = HumanTime.TimeAutoShort(totalTime - error, 2)
+    worseCase = HumanTime.TimeAutoShort(totalTime + error, 2)
+    majorityBestCase = HumanTime.TimeAutoShort(majorityTotalTime - majorityError, 2)
+    majorityWorseCase = HumanTime.TimeAutoShort(majorityTotalTime + majorityError, 2)
+    lowerCurrentEstimate = (capacity / 1000) / ((totalTime + error) / 3600)
+    upperCurrentEstimate = (capacity / 1000) / ((totalTime - error) / 3600)
+    print(100 * "-" + f'\nTotal time elapsed:  {totalTimeElapsedStr}\nAt this rate, a full charge should occur in {totalTimeStr} +/- {errorStr}.\nIt should go from 0% to 70% in {majorityTimeStr} +/- {majorityErrorStr}.\nFor a full charge, it will take between {bestCase} and {worseCase}.\nFor 0% - 70% it will take between {majorityBestCase} and {majorityWorseCase}\nIt will charge at 1% every {HumanTime.TimeAutoShort(totalTime/100, 2)}.\nIt will charge at 1 mAh every {HumanTime.TimeAutoShort(totalTime/capacity, 2)}.\nBetween {lowerCurrentEstimate:.2f} and {upperCurrentEstimate:.2f} Amps were delivered to the battery.\n' + 100 * "-")
+def main(capacity): # capacity is in mAh and is the size of the battery
+    "This function takes as a paramater the capacity of your battery in mAh"
+    UI = True
+    while UI:
+        try:
+            tmp = eval(input('What is the current percentage?\t'))
+            UI = False
+        except:
+            pass
+    mainH(tmp, capacity)
+def mainHelper():
+    while True:
+        ui = input("How much does your battery store in mAh?\t")
+        try:
+            ui = eval(ui)
+            break
+        except:
+            print("Your input cannot be recognized as a number")
+    main(ui)
+#main(2815) # For iPhone 12
+#main(1700) # For Puffco Peak Pro
+mainHelper() 
